fb96a525ee4ccbf984d73c38fd7a833c0e39c868
Giovanny Gordillo <gioggonzalez214@gmail.com>
2023-07-10 10:15:27 -0500

    added search game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 // GET https://api.rawg.io/api/platforms?key=9ebbf9679be0432fb3bc33d2de55d998
 // var querySelector = "https://api.rawg.io/api/games?key="
 var apiKey = "9ebbf9679be0432fb3bc33d2de55d998";
+var gameList = "https://api.rawg.io/api/games?key=" + apiKey
 
 fetch('https://api.rawg.io/api/games?key=9ebbf9679be0432fb3bc33d2de55d998', {
     cache: 'reload',
@@ -12,3 +13,12 @@ fetch('https://api.rawg.io/api/games?key=9ebbf9679be0432fb3bc33d2de55d998', {
     .then(function (data) {
         console.log(data);
     });
+
+    var searchEl = document.querySelector('#SearchBtn')
+function searchGame(event) {
+        event.preventDefault();
+        var game = document.querySelector("#query").ariaValueMax
+
+}
+
+searchEl.addEventListener("click", searchGame);
